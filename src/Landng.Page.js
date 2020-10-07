a326000665d781f70796891f3157261700af9ed8
piyumaldk <piyumaldk@gmail.com>
2020-10-07 17:55:47 +0530

    Login and Template

---
+++ src/Landng.Page.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { HashRouter, Switch } from 'react-router-dom';
+import './scss/style.scss';
+import './App.css';
+import TheSideBar from './SideBar';
+import TheHeader from './Header';
+function App() {
+  return (
+    <div className="App">
+      <HashRouter>
+          <React.Suspense>
+            <Switch>  
+              <div className="c-app c-default-layout">
+                <TheSideBar/>
+                <div className="c-wrapper">
+                  <TheHeader/>
+                  <div className="c-body">
+                    {/* <TheContent/> */}
+                  </div>
+                  {/* <TheFooter/> */}
+                </div>
+              </div>
+            </Switch>
+          </React.Suspense>
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.Page.js
@@ -0,0 +1,162 @@
+import React, { Component } from "react";
+import { Container, Spinner, Row, Col, Alert, Button, Form, FormGroup, Input } from 'reactstrap';
+
+export default class Login extends Component {
+   
+    
+    constructor(props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+    
+        this.state = {
+            loading: false,
+            alert: 0,
+            alertMsg: "",
+            number: "",
+            password: "",
+        };
+    }
+
+    // static propTypes = {
+    //     isAuthenticated: PropTypes.bool,
+    //     error: PropTypes.object.isRequired,
+    //     login: PropTypes.func.isRequired,
+    //     clearErrors: PropTypes.func.isRequired
+    // };
+
+    // componentDidUpdate(prevProps) {
+    //     const { error } = this.props;
+    //     if(error !== prevProps.error) {
+    //       //Check login errors
+    //       if(error.id === 'LOGIN_FAIL') {
+    //         this.setState({ 
+    //             alertMsg: error.msg.message,
+    //             alert: 1 
+    //         });
+    //       } 
+    //       else {
+    //         this.setState({ alertMsg: null });
+    //       }
+    //     }
+    // }
+
+    // closeAlert = () => {
+    //     this.setState({ alert: 0 });
+    // };
+
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+        
+    };
+
+    // reset = e => {
+    //     this.setState({
+    //         alert: 0,
+    //         alertMsg: "",
+    //         email: "",
+    //         password: "",
+    //     });
+    //     document.getElementById("form").reset();
+    // };
+
+    // validate = () => {
+    //     let error = false;
+    //     let alertMsg = "";
+    //     if (this.state.email.length < 1) {
+    //         error = true;
+    //         alertMsg = "Enter Email";
+    //     }
+    //     if (this.state.password.length < 1) {
+    //         error = true;
+    //         alertMsg = "Enter Password";
+    //     }
+    
+    //     this.setState({alertMsg: alertMsg});
+    //     return error;  
+    // }
+
+    onSubmit(e) {
+        // e.preventDefault();
+        // const error = this.validate();
+        // this.setState({ 
+        //     loading: true,
+        //     alert: 0
+        // });
+        // if(!error){
+        //     this.setState({ 
+        //         loading: false 
+        //     });
+        //     const { email, password } = this.state;
+        //     const user = {
+        //         email,
+        //         password
+        //     };
+        //     this.props.login(user);
+            
+        //         setTimeout(() => {  
+        //             window.location.reload(false);
+        //             }, 2000);   
+                          
+        // }
+        // else{
+        //     this.setState({ 
+        //         alert: 1,
+        //         loading: false 
+        //     });
+        // }  
+
+    }
+    
+    render(){
+        return (
+            <section>
+            <React.Fragment>
+            
+                <Container>
+                    <Row>
+                        <Col xs="0" sm="4"></Col>
+
+                        <Col  xs="12" sm="4">
+                            <br/><br/><br/><br/>
+                            <div className="center">
+                                <Form id="form" onSubmit={this.onSubmit}>
+                                   
+                                    <FormGroup>
+                                        <Input type="text" name="number" placeholder="Contact Number" id="number" onChange={this.onChange}/>
+                                    </FormGroup>
+
+                                    <FormGroup>
+                                        <Input type="password" name="password" placeholder="Password" id="password" onChange={this.onChange}/>
+                                    </FormGroup>
+                                    
+                                    <Row xs="12" sm="12">
+                                        <center>
+                                            { this.state.loading ?
+                                                <Spinner animation="border" className="spinner2" alignItems="center"/>
+                                            : null}
+                                        </center>
+                                    </Row>
+                                    
+                                    { this.state.alert === 1 ?
+                                        <Alert color="info" status={this.state.alert}>
+                                            {this.state.alertMsg}
+                                        </Alert>
+                                    : null }
+                                    <Row>
+                                        
+                                        <Col xs="12" sm="12">
+                                            <Button outline color="info" type="submit" length="100" block>Log In</Button>
+                                        </Col>
+                                        
+                                    </Row>        
+                                </Form>
+                            </div>
+                        </Col>
+                        <Col xs="0" sm="4"></Col>
+                    </Row>
+                </Container>
+            </React.Fragment>
+            </section>
+        );
+    }  
+}
