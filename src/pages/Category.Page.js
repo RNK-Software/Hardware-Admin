4b9e2fcc3b9080c704a1783064aa0f49cd94148d
piyumaldk <piyumaldk@gmail.com>
2020-10-23 17:03:37 +0530

    Add Category

---
+++ src/pages/Category.Page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddFormCategory from '../Components/AddFormCategory.Component';
+
+const CategoryPage = (props) => {
+    return(
+        <React.Fragment>
+            <AddFormCategory/>
+        </React.Fragment>
+    );
+};
+
+export default CategoryPage;
